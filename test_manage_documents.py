0e8ae7443b1394e1abbecfa6b75d7232eb0255d6
Tomas Krulis <krulis.tomas@seznam.cz>
2026-07-08 15:33:06 +0200

    Verify document created on the dashboard.

--- test_manage_documents.py
+++ test_manage_documents.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 from tasks.login.login import login_platform
 from tasks.app.documents import create_document
@@ -14,6 +14,9 @@ def test_manage_documents(setup_browser_instance: Page,
                                     get_login_credentials)
 
     dashboard_page.center_area.click(button = "right")
-    edit_document_form = create_document(dashboard_page.page,
-                    "automat_" + create_unique_string())
+
+    document_name = "automat_" + create_unique_string()
+    edit_document_form = create_document(dashboard_page.page, document_name)
     edit_document_form.close_edit_document_form()
+
+    expect(dashboard_page.center_area.get_by_text(document_name)).to_be_visible(timeout = 2_000)
