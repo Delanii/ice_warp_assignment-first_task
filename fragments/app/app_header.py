7ba62cc09fc2be50cec0b191e735f0c4476d4f99
Tomas Krulis <krulis.tomas@seznam.cz>
2026-07-08 09:58:46 +0200

    Refactor the header class slightly.

--- fragments/app/app_header.py
+++ fragments/app/app_header.py
@@ -2,7 +2,9 @@ from playwright.sync_api import Page
 
 class AppHeaderFragment:
 
-    def __init__(self, username: str):
+    def __init__(self, page: Page,
+                username: str):
 
+        self.page = page
         self.avatar_button_locator = ("//a[.//div[@class = 'component-avatar--image' and @data-email = '" +
                                           username + "']]")
--- test_create_document.py
+++ test_create_document.py
@@ -3,20 +3,16 @@ from playwright.sync_api import Page
 from fragments.app.app_header import AppHeaderFragment
 from tasks.login.login import login_platform
 
-from utils.utils import get_viewport_size
-
 def test_create_document(setup_browser_instance: Page,
                          get_login_credentials: dict) -> None:
     """
     """
 
-    app_header = AppHeaderFragment(get_login_credentials["username"])
+    app_header = AppHeaderFragment(setup_browser_instance, get_login_credentials["username"])
 
     dashboard_page = login_platform(setup_browser_instance,
                                     get_login_credentials,
                                     app_header)
-
-    viewport_size = get_viewport_size(dashboard_page.page)
 
     dashboard_page.center_area.click(button = "right")
     
